34f33e6428d5f6d3c41f8ff550315c221141e30e
Ziyad Tber <ziyad@novaprojectx.com>
2026-09-05 10:49:18 +0100

    Open CV in browser viewer

--- src/lib/cv.ts
+++ src/lib/cv.ts
@@ -1,3 +1,6 @@
-import cvUrl from '../assets/Ziyad_Tber_Software_Engineer_CV.docx?url';
+import cvDocumentUrl from '../assets/Ziyad_Tber_Software_Engineer_CV.docx?url';
+
+const deployedCvUrl = `https://ziyadtber.netlify.app${cvDocumentUrl}`;
+const cvUrl = `https://view.officeapps.live.com/op/view.aspx?src=${encodeURIComponent(deployedCvUrl)}`;
 
 export { cvUrl };
